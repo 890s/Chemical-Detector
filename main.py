9d70b026fbc0718b8ce8898e9ae3e9bc403ee761
890s <147666213+890s@users.noreply.github.com>
2025-04-01 12:12:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from SubstanceRecognizer import *
 import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+ttk.Label(root, text='Themed Label').pack()
+root.geometry('300x250')
+root.mainloop()
 
 substances = SubstanceRecognizer("physical_chemical_properties_of_organic_substances.csv").getSubstances()
 
 print(substances[0].getValues())
 
+#use this: https://www.pythontutorial.net/tkinter/tkinter-label/
