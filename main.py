0366d2883f5f2d87572345a9819a3fd2d858e6de
890s <147666213+890s@users.noreply.github.com>
2025-04-01 12:03:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from SubstanceRecognizer import *
 import tkinter as tk
 
-substances = SubstanceRecognizer("C:\\Users\\200448984\\PycharmProjects\\PythonProject\\physical_chemical_properties_of_organic_substances.csv").getSubstances()
+substances = SubstanceRecognizer("physical_chemical_properties_of_organic_substances.csv").getSubstances()
 
 print(substances[0].getValues())
 
