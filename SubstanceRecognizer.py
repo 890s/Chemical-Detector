d0b61ca455c23f7fda228359a3c48a2c86f8da1c
890s <147666213+890s@users.noreply.github.com>
2025-04-15 11:52:38 -0400

    Update SubstanceRecognizer.py

--- SubstanceRecognizer.py
+++ SubstanceRecognizer.py
@@ -9,4 +9,41 @@ class SubstanceRecognizer:
     def getSubstances(self):
         return self.substances
 
+    def findSimilarity(self, sub1, sub2):
+        count = 0
+        num = 0
+        for i in range(2,len(sub1.getValues())):
+            if (sub1[i]=="" or sub2[i]==""):
+                continue
+            elif (sub1[i]==sub2[i]):
+                count += 1
 
+            num += 1
+
+        return count/num
+
+    def findMostSimilar(self, sub):
+        maxSimilarity = 0
+        prediction = ""
+        for substance in self.substances:
+            if (self.findSimilarity(sub,substance)>max):
+                maxSimilarity = self.findSimilarity(sub, substance)
+                prediction = sub.getName()
+
+        return (prediction, maxSimilarity)
+
+    def nameToSubstance(self, name):
+        for substance in self.substances:
+            if (name==substance.getName()):
+                return substance
+
+    def findKMostSimilar(self, sub, k):
+        substancesBackup = self.substances
+        predictions = []
+        for i in range(k):
+            pred = self.findMostSimilar(sub)[0]
+            self.substances = self.substances.remove(self.nameToSubstance(pred))
+            predictions.append(pred)
+            
+        self.substances = substancesBackup
+        return predictions
